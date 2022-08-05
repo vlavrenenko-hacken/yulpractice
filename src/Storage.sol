f2da19f747d34de87f71c35cca221c066b5ea6e5
Viktor Lavrenenko <lavrenenkoviktoren@gmail.com>
2022-08-05 16:58:37 +0300

    Add getOffset function

--- src/Storage.sol
+++ src/Storage.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.13;
 contract Storage {
     uint private a = 100;
     uint private b = 1341;
+    uint128 private c = 1;
+    uint128 private d = 2;
 
     function getStorageLocation() external pure returns(uint, uint) {
         uint slotA;
@@ -25,5 +27,12 @@ contract Storage {
         }
         
         return value;
+    }
+
+    function getOffset() external pure returns (uint offsetC, uint offsetD) {
+        assembly {
+            offsetC := c.offset
+            offsetD := d.offset
+        }
     }
 }
